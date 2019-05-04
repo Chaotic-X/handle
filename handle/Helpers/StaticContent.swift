b947e3c63072c25ac32107d727507b0c5c8a039b
Alex Lundquist <alex@alexludnquist.com>
2019-05-04 12:50:42 -0600

    Added in PhotoSelector on PostStoryBoard

---
+++ handle/Helpers/StaticContent.swift
@@ -0,0 +1,26 @@
+//
+//  StaticContent.swift
+//  handle
+//
+//  Created by Alex Lundquist on 5/4/19.
+//  Copyright © 2019 Alex Lundquist. All rights reserved.
+//
+
+import UIKit
+
+class StaticContent {
+  static let shared = StaticContent()
+  private init() {}
+  
+
+  var postContent: [PostContent] = []
+  
+  func createPost(caption: String, crPhoto: UIImage) -> [PostContent]{
+    let myPost = PostContent(fbCaption: caption, fbPhoto: crPhoto)
+    return [myPost]
+  }
+}
+struct PostContent {
+  let fbCaption: String
+  let fbPhoto: UIImage
+}
--- handle/ViewControllers/SelectPhotoViewController.swift
+++ handle/ViewControllers/SelectPhotoViewController.swift
@@ -8,26 +8,50 @@
 
 import UIKit
 
-class SelectPhotoViewController: UIViewController {
-
+class SelectPhotoViewController: UIViewController, UITextFieldDelegate  {
+  @IBOutlet weak var captionTextField: UITextField!
+  var selectedImage: UIImage?
+  var postObject: [PostContent]?
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        captionTextField.delegate = self
+      
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
+    selectedImage = nil
+    captionTextField.text = nil
+  }
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if segue.identifier == "toPhotoSelector" {
+      let photoSelector = segue.destination as? PhotoSelectorViewController
+      photoSelector?.delegate = self
     }
-    */
+  }
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    textField.resignFirstResponder()
+    return true
+  }
+  @IBAction func postToFBbuttonTapped(_ sender: UIButton) {
+    guard let photoToPost = selectedImage,
+      let caption = captionTextField.text,
+      !caption.isEmpty else {
+        return
+    }
+    postObject = StaticContent.shared.createPost(caption: caption, crPhoto: photoToPost)
+   self.dismiss(animated: true, completion: nil)
+  }
   @IBAction func selectPhotoCancelTapped(_ sender: Any) {
     self.dismiss(animated: true, completion: nil)
   }
-  
-}
+}//End of Class
+
+//MARK: -Extensions
+
+extension SelectPhotoViewController: PhotoSelectorViewControllerDelegate {
+  func photoSelectorViewControllerSelected(image: UIImage) {
+    captionTextField.resignFirstResponder()
+    selectedImage = image
+  }
+}//End of Extension
