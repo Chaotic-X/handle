6fcc10dce144936f3e9faf3ed0de67e4901bb346
statusqua <andriyfilenko@gmail.com>
2019-05-07 12:26:30 -0400

    Adds post record model and controller

---
+++ handle/Firebase/RecordKey.swift
@@ -0,0 +1,18 @@
+//
+//  MessageKey.swift
+//  handle
+//
+//  Created by Drew on 5/6/19.
+//  Copyright © 2019 Alex Lundquist. All rights reserved.
+//
+
+import Foundation
+
+struct RecordKey {
+    static let message = "message"
+    static let creatorUid = "creatorUid"
+    static let creationTimestamp = "creationTimestamp"
+    static let postingTimestamp = "postingTimestamp"
+    static let imageUrl = "imageUrl"
+    static let uid = "uid"
+}
