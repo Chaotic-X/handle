53b425511111832b96cf8047b0fcd5f148a03ef1
statusqua <andriyfilenko@gmail.com>
2019-05-09 12:55:30 -0400

    Fixes file references and removes duplication from FirebaseStorageController

--- handle/Models/InternalUserKey.swift
+++ handle/Models/InternalUserKey.swift
@@ -2,14 +2,8 @@
 //  InternalUserKey.swift
 //  handle
 //
-//  Created by Drew on 5/8/19.
+//  Created by Drew on 5/9/19.
 //  Copyright © 2019 Alex Lundquist. All rights reserved.
 //
 
 import Foundation
-
-struct InternalUserKey {
-    static let emailAddress = "emailAddress"
-    static let recordsUids = "recorsdUids"
-    static let uid = "uid"
-}
